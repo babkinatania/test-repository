afd54f45510e13187d972c54ae2ea280665d2cf2
babkinatania <babkina.tania@gmail.com>
2016-11-17 23:29:43 +0100

    Task 3
    Задание 3. Сделайте сценарий логина

---
+++ sel-1_task1/sel-1_task1/TestLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace sel_1_task1
+{
+    [TestFixture]
+    public class TestLogin
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        [Test]
+        public void TestLoginPage()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
