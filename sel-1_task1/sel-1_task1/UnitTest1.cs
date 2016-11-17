9e2ca8a3c4aec0777bb28b7eea1245bb356bd63d
babkinatania <babkina.tania@gmail.com>
2016-11-17 19:57:58 +0100

    Task 1
    Задание 1. Подготовьте инфраструктуру

---
+++ sel-1_task1/sel-1_task1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace sel_1_task1
+{
+    [TestFixture]
+    public class OpenCloseBrowser
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void OpenBrowser()
+        {
+            driver.Url = "http://www.google.com/";
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
